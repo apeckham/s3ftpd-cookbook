e753e15ce40608a292b514659ff46c897d0c38aa
Aaron Peckham <apeckham@gmail.com>
2012-06-14 10:25:07 -0700

    start writing chef script for installing and running s3ftpd

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,47 @@
 # Cookbook Name:: s3ftpd
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, apeckham
 #
-# All rights reserved - Do Not Redistribute
-#
+
+include_recipe "git"
+
+git "/usr/share/s3ftpd" do
+  repository "git://github.com/apeckham/s3ftpd.git"
+  revision "master"
+  action :sync
+end
+
+gem_package "bundler" do
+  action :install
+end
+
+execute "install dependencies" do
+  command "bundle install"
+  cwd "/usr/share/s3ftpd"
+end
+
+service "s3ftpd" do
+  provider Chef::Provider::Service::Upstart
+
+  restart_command "stop s3ftpd; start s3ftpd"
+  start_command "start s3ftpd"
+  stop_command "stop s3ftpd"
+
+  supports :restart => true, :start => true, :stop => true
+end
+
+directory "/usr/share/s3ftpd/scripts" do
+  action :create
+end
+
+template "/usr/share/s3ftpd/scripts/start" do
+  source "upstart.start.erb"
+  mode 0755
+
+  notifies :restart, resources(:service => "s3ftpd")
+end
+
+service "s3ftpd" do
+  action [ :enable, :start ]
+end
