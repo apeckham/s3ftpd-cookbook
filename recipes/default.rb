10e8e036ab166ff7ebd12f2c700dde3d5673a64a
Aaron Peckham <apeckham@gmail.com>
2012-07-26 17:32:23 -0700

    use inotifywait to upload files to s3 when they are closed in /home/upload/incoming

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,60 +1,79 @@
-#
-# Cookbook Name:: s3ftpd
-# Recipe:: default
-#
-# Copyright 2012, apeckham
-#
-
-include_recipe "git"
 include_recipe "build-essential"
 
-git "/usr/share/s3ftpd" do
-  repository "git://github.com/apeckham/s3ftpd.git"
-  revision "master"
-  action :sync
+begin
+  apt_package "vsftpd" do
+    action :install
+  end
+
+  service "vsftpd" do
+    supports restart: true
+    action [:enable, :start]
+  end
+
+  cookbook_file "/etc/vsftpd.conf" do
+    source "vsftpd.conf"
+    mode 0644
+    notifies :restart, resources(service: "vsftpd")
+  end
 end
 
-apt_package "libssl-dev" do
-  action :install
+begin
+  cookbook_file "/etc/shells" do
+    source "shells"
+    mode 0644
+  end
+
+  chef_gem "ruby-shadow"
+
+  user "upload" do
+    password node[:s3ftpd][:password]
+    home "/home/upload"
+    shell "/bin/false"
+  end
+
+  directory "/home/upload" do
+    user "upload"
+    group "upload"
+    mode 0555
+  end
+
+  directory "/home/upload/incoming" do
+    user "upload"
+    group "upload"
+    mode 0755
+  end
 end
 
-gem_package "bundler" do
-  action :install
+begin
+  apt_package "s3cmd" do
+    action :install
+  end
+
+  template "/usr/share/s3cfg" do
+    source "s3cfg.erb"
+    mode 0644
+  end
 end
 
-execute "install dependencies" do
-  command "bundle install"
-  cwd "/usr/share/s3ftpd"
-end
+begin
+  apt_package "inotify-tools" do
+    action :install
+  end
 
-service "s3ftpd" do
-  provider Chef::Provider::Service::Upstart
+  service "inotify-s3" do
+    provider Chef::Provider::Service::Upstart
 
-  restart_command "stop s3ftpd; start s3ftpd"
-  start_command "start s3ftpd"
-  stop_command "stop s3ftpd"
+    restart_command "stop inotify-s3 && start inotify-s3"
+    start_command "start inotify-s3"
+    stop_command "stop inotify-s3"
 
-  supports :restart => true, :start => true, :stop => true
-end
+    supports :restart => true, :start => true, :stop => true
+  end
 
-directory "/usr/share/s3ftpd/scripts" do
-  action :create
-end
+  template "/etc/init/inotify-s3.conf" do
+    source "upstart.conf.erb"
+    mode 0644
 
-template "/usr/share/s3ftpd/scripts/start" do
-  source "upstart.start.erb"
-  mode 0755
-
-  notifies :restart, resources(:service => "s3ftpd")
-end
-
-cookbook_file "/etc/init/s3ftpd.conf" do
-  source "upstart.conf"
-  mode 0644
-
-  notifies :restart, resources(:service => "s3ftpd")
-end
-
-service "s3ftpd" do
-  action [ :enable, :start ]
+    notifies :restart, resources(:service => "inotify-s3")
+  end
 end
